d2f510ba7458c98e3bd298a455748bb5b58a97ab
Tjorven Verbeeck <tjorven.verbeeck@outlook.be>
2024-06-15 15:45:21 +0200

    Add state files for different screens

---
+++ FirstGame/FirstGame/States/GameState.cs
@@ -0,0 +1,63 @@
+﻿//using FirstGame.Characters;
+//using Microsoft.Xna.Framework;
+//using Microsoft.Xna.Framework.Graphics;
+//using Microsoft.Xna.Framework.Input;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace FirstGame.States
+//{
+//    public class Game1 : Game
+//    {
+//        private GraphicsDeviceManager _graphics;
+//        private SpriteBatch _spriteBatch;
+
+//        private Texture2D _heroTexture;
+
+//        public Game1()
+//        {
+//            _graphics = new GraphicsDeviceManager(this);
+//            Content.RootDirectory = "Content";
+//            IsMouseVisible = true;
+//        }
+
+//        private Hero hero;
+//        private int scale = 1;
+//        protected override void Initialize()
+//        {
+//            // TODO: Add your initialization logic here
+//            base.Initialize();
+//            hero = new Hero(_heroTexture, new Input.KeyboardReader());
+//        }
+
+//        protected override void LoadContent()
+//        {
+//            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+//            // TODO: use this.Content to load your game content here
+//            _heroTexture = Content.Load<Texture2D>("black-mage(Hero)");
+//        }
+
+//        protected override void Update(GameTime gameTime)
+//        {
+//            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+//                Exit();
+
+//            // TODO: Add your update logic here
+//            hero.update(gameTime);
+//            base.Update(gameTime);
+//        }
+
+//        protected override void Draw(GameTime gameTime)
+//        {
+//            GraphicsDevice.Clear(Color.CornflowerBlue);
+//            _spriteBatch.Begin();
+//            hero.draw(_spriteBatch);
+//            _spriteBatch.End();
+//            base.Draw(gameTime);
+//        }
+//    }
+//}

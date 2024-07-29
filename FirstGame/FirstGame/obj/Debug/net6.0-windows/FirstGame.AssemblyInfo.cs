bfbc44c2a862d9c6a5a915ebebc2106f7dfe3b33
Tjorven Verbeeck <tjorven.verbeeck@outlook.be>
2024-07-29 19:08:37 +0200

    required commit

--- FirstGame/FirstGame/obj/Debug/net6.0-windows/FirstGame.AssemblyInfo.cs
+++ FirstGame/FirstGame/obj/Debug/net6.0-windows/FirstGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40324b002ebb73fc5937205c75acc2cdcfe6aeb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+087a3c5a1f54716025f77a0fb5a4168ca1df81ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
